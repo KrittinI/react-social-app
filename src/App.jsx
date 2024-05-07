dbacc603719f72e3d8e9355812f2700e29ce3f95
KrittinI <krittin.ikk@gmail.com>
2024-05-07 15:03:28 +0700

    finished login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Router from "./routes/Router"
+import { CssBaseline } from "@mui/material"
 import ThemeContextProvider from "./context/ThemeContext"
 import PostContextProvider from "./context/PostContext"
 import AuthContextProvider from "./context/AuthenContext"
@@ -6,13 +7,15 @@ import AuthContextProvider from "./context/AuthenContext"
 function App() {
 
   return (
-    <ThemeContextProvider>
-      <AuthContextProvider>
-        <PostContextProvider>
-          <Router />
-        </PostContextProvider>
-      </AuthContextProvider>
-    </ThemeContextProvider>
+    <CssBaseline>
+      <ThemeContextProvider>
+        <AuthContextProvider>
+          <PostContextProvider>
+            <Router />
+          </PostContextProvider>
+        </AuthContextProvider>
+      </ThemeContextProvider>
+    </CssBaseline >
   )
 }
 
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,13 +1,70 @@
-import { Link } from "react-router-dom"
+import { Box, Button, TextField, Typography, Divider } from "@mui/material"
+
 function LoginPage() {
     return (
-        <div>
-            <h1>
-                LoginPage
-            </h1>
-            <Link to="/">HomePage</Link   >
-            <Link to="/profile">ProfilePage</Link >
-        </div>
+        <Box component='main'
+            height='100vh'
+            display='flex'
+            justifyContent='space-between'
+        >
+            <Box
+                component='section'
+                display='flex'
+                flexDirection='column'
+                justifyContent='center'
+                p='20px'
+                flex={1}
+            >
+                <Typography variant="h3" color='#4D8ED3' fontWeight={900}>FakeBuck</Typography>
+                <Typography variant="subtitle" fontWeight={500}>Connect with friend and the world</Typography>
+            </Box>
+            <Box
+                component='section'
+                display='flex'
+                flexDirection='column'
+                justifyContent='center'
+                p='20px'
+                flex={1}
+            >
+                <Box
+                    component='form'
+                    display='flex'
+                    flexDirection='column'
+                    gap='10px' padding='8px'
+                    backgroundColor='white'
+                    borderRadius='4px'
+                >
+                    <TextField
+                        type='text'
+                        variant="outlined"
+                        label="email or Phone number"
+                        size="small"
+                    />
+                    <TextField
+                        type='password'
+                        variant="outlined"
+                        label='password'
+                        size="small"
+                    />
+                    <Button type='submit' variant="contained" color="primary">
+                        Login
+                    </Button>
+                    <Button
+                        type='button'
+                        variant="text"
+                        disableElevation
+                        color="primary"
+                        sx={{ fontSize: '12px' }}
+                    >
+                        FORGOT PASSWORD?
+                    </Button>
+                    <Divider />
+                    <Button type='button' variant="contained" color="success">
+                        Create New Account
+                    </Button>
+                </Box>
+            </Box>
+        </Box >
     )
 }
 
